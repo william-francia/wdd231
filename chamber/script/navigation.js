1bc27fa440ef439bb940b561230be8e79b30b568
william-francia <wfranciapoma@byupathway.edu>
2025-11-12 17:41:04 -0400

    Changes

---
+++ chamber/script/navigation.js
@@ -0,0 +1,11 @@
+const navbuttom = document.querySelector('#hamburgeridmenu');
+const navlinks = document.querySelector('#navidmenu');
+
+// Toggle the show class off and on
+navbuttom.addEventListener('click', () => {
+    navbuttom.classList.toggle('show');
+    navlinks.classList.toggle('show');
+});
+
+
+
